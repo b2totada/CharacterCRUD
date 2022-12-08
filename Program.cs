a74c2a27469f0a6a83f4cbc9ad6fb12433398d18
Tóth Ádám <totha@griffsoft.hu>
2022-12-08 16:15:25 +0100

    add both

--- Program.cs
+++ Program.cs
@@ -8,8 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 //db connection
+//builder.Services.AddDbContext<DataContext>(options =>
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerAtWorkConnection")));
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerAtWorkConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
